323bb6443f3fe9e8e4dc9a5c382d12138b397046
francids <francisco.mc.ids@gmail.com>
2024-09-24 10:50:09 -0400

    chore: Change the welcome note page style

--- src/app/pages/notes/note-welcome.component.ts
+++ src/app/pages/notes/note-welcome.component.ts
@@ -2,6 +2,12 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'note-welcome',
-  template: `<p>Bienvenido a la página de notas</p>`
+  template: `<p>¡Bienvenido a la página de notas!</p>`,
+  styles: [`
+    p { 
+      text-align: center;
+      font-size: 1.1em;
+    }
+   `]
 })
 export class NoteWelcomeComponent { }
