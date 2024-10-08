bf27e0068f3a7f7f31f03434b38500aefb7c71d8
francids <francisco.mc.ids@gmail.com>
2024-10-08 09:07:09 -0400

    feat: Configure Capacitor for Android app

---
+++ android/app/src/main/java/com/productiva/mente/MainActivity.java
@@ -0,0 +1,5 @@
+package com.productiva.mente;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,10 +4,11 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideServiceWorker } from '@angular/service-worker';
+import { provideIonicAngular } from '@ionic/angular/standalone';
 
 export const appConfig: ApplicationConfig = {
   providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideAnimationsAsync(), provideServiceWorker('ngsw-worker.js', {
     enabled: !isDevMode(),
     registrationStrategy: 'registerWhenStable:30000'
-  })],
+  }), provideIonicAngular({})],
 };
