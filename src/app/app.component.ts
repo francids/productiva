0901a4ad429de31ab6ff666af2460ef2d022f4f6
francids <francisco.mc.ids@gmail.com>
2024-10-14 14:47:55 -0400

    ui: Add sidenav to app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
 import 'zone.js/plugins/zone-patch-rxjs';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterLink, RouterOutlet, MatToolbarModule, MatButtonModule],
+  imports: [RouterLink, RouterOutlet, MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
