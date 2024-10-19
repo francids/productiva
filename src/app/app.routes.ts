8d90fd6641731de871a6509cf6a7325c35cdb4f8
francids <francisco.mc.ids@gmail.com>
2024-10-19 16:41:52 -0400

    chore: Adding initial styles and components for the home page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 
+import { HomeComponent } from './pages/home/home.component';
 import { notesRoutes } from './pages/notes/notes.routes';
 import { TasksComponent } from './pages/tasks/tasks.component';
 
 export const routes: Routes = [
+  { path: "", component: HomeComponent },
   { path: 'notes', children: notesRoutes },
   { path: 'tasks', component: TasksComponent }
 ];
