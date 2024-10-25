d5af460d91618dc92e9a353c1adb2ec3fd1a1bff
francids <francisco.mc.ids@gmail.com>
2024-10-25 11:16:50 -0400

    ui: Add settings page and dark theme

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,9 +3,11 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { notesRoutes } from './pages/notes/notes.routes';
 import { TasksComponent } from './pages/tasks/tasks.component';
+import { SettingsComponent } from './pages/settings/settings.component';
 
 export const routes: Routes = [
   { path: "", component: HomeComponent },
   { path: 'notes', children: notesRoutes },
-  { path: 'tasks', component: TasksComponent }
+  { path: 'tasks', component: TasksComponent },
+  { path: "settings", component: SettingsComponent },
 ];
---
+++ src/app/pages/settings/settings.component.ts
@@ -0,0 +1,27 @@
+import { Component, Renderer2 } from "@angular/core";
+import { TitleService } from '../../services/title.service';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+
+@Component({
+  selector: "settings-page",
+  standalone: true,
+  templateUrl: "./settings.component.html",
+  styleUrls: ["./settings.component.scss"],
+  imports: [MatSlideToggleModule]
+})
+export class SettingsComponent {
+  constructor(
+    private titleService: TitleService,
+    private renderer: Renderer2,
+  ) {
+    this.titleService.updateTitle('Configuración');
+  }
+
+  toggleDarkMode(event: any): void {
+    if (event.checked) {
+      this.renderer.addClass(document.body, 'darkmode');
+    } else {
+      this.renderer.removeClass(document.body, 'darkmode');
+    }
+  }
+}
