6daadcb6cd7ec33f5caa97d736c87267920779b0
francids <francisco.mc.ids@gmail.com>
2024-10-28 16:40:23 -0400

    feat: Configure Electron for Desktop app

---
+++ main.js
@@ -0,0 +1,36 @@
+import { app, BrowserWindow } from 'electron';
+import * as path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+function createWindow() {
+  const mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+      contextIsolation: true,
+      enableRemoteModule: true
+    },
+  });
+
+  mainWindow.setMenu(null);
+
+  mainWindow.loadURL(`file://${__dirname}/dist/productiva-mente/browser/index.html`);
+}
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
