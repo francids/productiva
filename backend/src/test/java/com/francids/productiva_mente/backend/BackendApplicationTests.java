e66a0afbe9ee330ea65ce061d8d52330835646ef
francids <francisco.mc.ids@gmail.com>
2025-03-08 12:05:50 -0400

    feat(backend): Initialize backend project structure with Gradle

---
+++ backend/src/test/java/com/francids/productiva_mente/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.francids.productiva_mente.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
